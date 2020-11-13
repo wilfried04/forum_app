0951d1f8a053c3392b48da80893d090a4e3bf24c
wilromeo <wilfriedkp.romeo@mail.com>
2020-11-13 14:02:36 +0100

    fixe error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#User.new({ :email => 'admin@example.com', :password => 'password', :password_confirmation => 'password'}).save
+newuser = User.new({ :email => 'admin@example.com', 
+    :password => 'password', 
+    :password_confirmation => 'password'})
+#newuser.skip_confirmation!
+newuser.save
